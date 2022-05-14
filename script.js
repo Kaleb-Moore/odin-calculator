660aad7002c7725476d1dc2038fdd3055bbcb547
Kaleb Moore <k4l3bmoore@gmail.com>
2022-05-14 11:15:29 -0600

    Update Calculator now stores last number and opertator

--- script.js
+++ script.js
@@ -2,13 +2,25 @@ let results = document.querySelector('.results');
 
 let buttons = document.querySelectorAll('button');
 
+let numArray = [];
+
 buttons.forEach(button => {
     button.addEventListener('click', e => {
-        if (results.value == '0') {
+        if (results.value === '0') {
             results.value = e.target.innerText;
+        }else if (e.target.innerText === '-' || e.target.innerText === '/' || e.target.innerText === '*' || e.target.innerText === '+') {
+            numArray.push(makeNum(results.value, e.target.innerText));
+            results.value = '0';
         } else {
             results.value += e.target.innerText;
         }
 
     })
 })
+
+function makeNum(num, operator) {
+    return {
+        num,
+        operator,
+    };
+}
