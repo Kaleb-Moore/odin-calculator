10cbaa570306affd53137d71fda577152c7e3591
Kaleb Moore <k4l3bmoore@gmail.com>
2022-05-14 12:11:19 -0600

    Create Clear function

--- script.js
+++ script.js
@@ -2,19 +2,40 @@ let results = document.querySelector('.results');
 
 let buttons = document.querySelectorAll('button');
 
-let numArray = [];
+let numObj = {
+    num: 0,
+    operator: '',
+};
+
+let total = 0;
 
 buttons.forEach(button => {
     button.addEventListener('click', e => {
-        if (results.value === '0') {
+        //just started using calculator
+        if (results.value === '0' || results.value == total) {
             results.value = e.target.innerText;
-        }else if (e.target.innerText === '-' || e.target.innerText === '/' || e.target.innerText === '*' || e.target.innerText === '+') {
-            numArray.push(makeNum(results.value, e.target.innerText));
-            results.value = '0';
+
+        //if an operator was clicked and there is already a num in obj
+        } else if ((e.target.innerText === '-' || e.target.innerText === '/' 
+        || e.target.innerText === '*' || e.target.innerText === '+') && numObj.num != "0"){
+            doMath(numObj, results.value);
+            results.value = total;
+        
+        //it an operator was clicked and there is no num in obj
+        } else if (e.target.innerText === '-' || e.target.innerText === '/' 
+        || e.target.innerText === '*' || e.target.innerText === '+' && numObj.num == 0) {
+            numObj = (makeNum(results.value, e.target.innerText));
+            total = parseInt(numObj.num)
+            results.value = total;
+        
+        //adds number to end of value to make a bigger number
         } else {
             results.value += e.target.innerText;
         }
 
+        if (e.target.innerText === "Clear") {
+            clear();
+        }
     })
 })
 
@@ -23,4 +44,23 @@ function makeNum(num, operator) {
         num,
         operator,
     };
+}
+
+function doMath(obj, nextVal) {
+    if (obj.operator === '/') {
+        total = parseInt(obj.num) / parseInt(nextVal);
+    } else if (obj.operator === '*') {
+        total = parseInt(obj.num) * parseInt(nextVal);
+    } else if (obj.operator === '-') {
+        total = parseInt(obj.num) - parseInt(nextVal);
+    } else if (obj.operator === '+') {
+        total = parseInt(obj.num) + parseInt(nextVal);
+    };
+}
+
+function clear() {
+    numObj.num = 0;
+    numObj.operator = '';
+    total = 0;
+    results.value = 0;
 }
